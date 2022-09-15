de393532f86c79329dd2472c33041d17be210562
ananyakar2012 <annu.titli@gmail.com>
2022-09-15 20:26:33 +0530

    upload-node_modules

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 const fs = require('fs')
 const argv = require('yargs').argv
+const express = require('express')
 var filename = argv.filename
 const folderName = './file';
-
-fs.readFile(`${folderName}/filename.txt`, (err, data) => {
-    if (err) {
-        console.log(err) 
-    } else {
-        
-        if (data.toString().includes(filename) === true) {
-            console.log('Please give new filename')
+if (filename) {
+    fs.readFile(`${folderName}/filename.txt`, (err, data) => {
+        if (err) {
+            console.log(err) 
         } else {
-            var fileNameArr = []
-            fileNameArr.push(filename)
+            
+            if (data.toString().includes(filename) === true) {
+                console.log('Please give new filename')
+            } else {
+                var fileNameArr = []
+                fileNameArr.push(filename)
 
-            fs.appendFile(`${folderName}/filename.txt`, fileNameArr.toString(), (err) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    fs.writeFile(`${folderName}/${filename}`, 'You are awesome', (err) => {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            console.log(filename+' created')
-                        }
-                    })
-                }
-            })
+                fs.appendFile(`${folderName}/filename.txt`, fileNameArr.toString(), (err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        fs.writeFile(`${folderName}/${filename}`, 'You are awesome', (err) => {
+                            if (err) {
+                                console.log(err)
+                            } else {
+                                console.log(filename+' created')
+                            }
+                        })
+                    }
+                })
+            }
+            
         }
-        
-    }
-})
+    })
+} else {
+    const app = express()
+    app.get('/api', (req, res) => {
+        fs.readFile(`${folderName}/sample.json`, 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(JSON.parse(data))
+            }
+        })
+    })
+}
